0968fb379e9511e644f988569d6fe4030aee37a8
Gabriela Yasmin Vidales Ayala <158981222+GabrielaVidales@users.noreply.github.com>
2026-05-04 13:10:09 -0600

    Implement logo loading as base64 and update display
    Added a function to load a logo as a base64 data-URI and updated the image display logic to use this function.

--- smilx_parameters.py
+++ smilx_parameters.py
@@ -1,102 +1,130 @@
 import re
+import base64
+import os
 from smilx_chemistry_tools import get_unsaturations, get_hdi
 import streamlit as st
 
+
+def _get_logo_base64(filename: str) -> str:
+    """Load a PNG logo from the same directory as this file and return base64 data-URI."""
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    # Fallback chain: requested file → logo_smilx_fixed.png → logo_smilx.png
+    candidates = [filename, "logo_smilx_fixed.png", "logo_smilx.png"]
+    for name in candidates:
+        path = os.path.join(base_dir, name)
+        if os.path.isfile(path):
+            with open(path, "rb") as f:
+                data = base64.b64encode(f.read()).decode()
+            return f"data:image/png;base64,{data}"
+    return ""  # no logo found – render nothing
+
+
 class initial_parameters:
-#----------------------------------------------------------------------------------- Section 0
-  def __init__(self):
+# ----------------------------------------------------------------------------------- Section 0
+    def __init__(self):
 
-    col1, col2, col3 = st.columns(3)
-    with col2:
-      st.image('logo_smilx_dark.png', use_container_width=True)
-    
-    st.markdown(
-      """
-      <div style="text-align: center;">
-        "Grammar-Driven SMILES Standardization with TokenSMILES" by Luis Armando Gonzalez-Ortiz, Lisset Noriega,<br>
-        Filiberto Ortiz, Gabriela Vidales-Ayala, Emmanuel Soberanis, Amilcar Meneses, Alan Aspuru-Guzik, and Gabriel Merino.<br>
-        Centro de Investigación y Estudios Avanzados (Cinvestav) Mérida<br>
-        GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 Copyright (C) 2007 Free Software Foundation <br>
-          <br>
-      </div>
-      """,
+        col1, col2, col3 = st.columns(3)
+        with col2:
+            # Use base64 embedding to avoid Streamlit Cloud MediaFileStorageError.
+            # The original call  st.image('logo_smilx_dark.png', ...)  failed because
+            # that file does not exist in the repo; we fall back to logo_smilx_fixed.png /
+            # logo_smilx.png and serve them inline so no server-side file storage is needed.
+            logo_uri = _get_logo_base64("logo_smilx_dark.png")
+            if logo_uri:
+                st.markdown(
+                    f'<img src="{logo_uri}" style="width:100%;height:auto;" />',
+                    unsafe_allow_html=True,
+                )
 
-      unsafe_allow_html=True
-    )
-    self.ask_molecular_formula()       # Enter molecular formula               e.g. C6H6, C2H5NO2
-    self.write_name_output_file()      # Write name output file                "C6H6_Structural_isomers_without_carbenes"
-    self.get_syntax_rules()            # Get syntax_rules                      [[0], [0],[0, 1], [0, 1], [0]]
-    self.get_cycles_pi_systems()       # Get syntax_rules                      [[0], [0],[0, 1], [0, 1], [0]]
-#----------------------------------------------------------------------------------- Section 1
-  def count_atoms_from_molecular_formula(self, molecular_formula):
-    self.n_heavy_atoms = 0
-    for element, atoms in zip(molecular_formula.keys(), molecular_formula.values()):
-      if element not in {'hdi', 'H'}:
-        self.n_heavy_atoms += atoms
-        
-  def ask_molecular_formula(self):
-    try:
-      self.str_molecular_formula = st.text_input("Enter the molecular formula (e.g. C6H6, C2H5NO2): ", value="C6H14", key="first")
-      self.opt_carbenes = st.checkbox("Search with carbenes")
-      self.get_molecular_formula()
-      self.is_valid_molecular_formula = True
-      self.count_atoms_from_molecular_formula(self.molecular_formula)
-    except:
-      pass
-  
+        st.markdown(
+            """
+            <div style="text-align: center;">
+              "Grammar-Driven SMILES Standardization with TokenSMILES" by Luis Armando Gonzalez-Ortiz, Lisset Noriega,<br>
+              Filiberto Ortiz, Gabriela Vidales-Ayala, Emmanuel Soberanis, Amilcar Meneses, Alan Aspuru-Guzik, and Gabriel Merino.<br>
+              Centro de Investigación y Estudios Avanzados (Cinvestav) Mérida<br>
+              GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007 Copyright (C) 2007 Free Software Foundation <br>
+                <br>
+            </div>
+            """,
+            unsafe_allow_html=True,
+        )
+        self.ask_molecular_formula()       # Enter molecular formula               e.g. C6H6, C2H5NO2
+        self.write_name_output_file()      # Write name output file                "C6H6_Structural_isomers_without_carbenes"
+        self.get_syntax_rules()            # Get syntax_rules                      [[0], [0],[0, 1], [0, 1], [0]]
+        self.get_cycles_pi_systems()       # Get syntax_rules                      [[0], [0],[0, 1], [0, 1], [0]]
 
-  def get_molecular_formula(self):
-    elements_y_atoms = re.findall(r'([A-Z][a-z]*)(\d*)', self.str_molecular_formula)
-    if elements_y_atoms != []:
-        self.molecular_formula = dict()
-        for i_tuple in elements_y_atoms:
-            if i_tuple[1] == '':
-                self.molecular_formula[i_tuple[0]] = 1
-            else:
-                self.molecular_formula[i_tuple[0]] = int(i_tuple[1])
-        for i_element in 'C','H','N','O','S','B','P','F','Cl','Br','I':
-            if i_element not in self.molecular_formula:
-                self.molecular_formula[i_element] = 0
-        self.molecular_formula['hdi'] = get_hdi(self.molecular_formula)
-    else:
-        self.molecular_formula = None
-      
-    # Get output vector elements
-    self.out_fm = []
-    for i_element in 'C','N','O','S','B','P','F','Cl','Br','I':
-        self.out_fm.append(self.molecular_formula[i_element])
+# ----------------------------------------------------------------------------------- Section 1
+    def count_atoms_from_molecular_formula(self, molecular_formula):
+        self.n_heavy_atoms = 0
+        for element, atoms in zip(molecular_formula.keys(), molecular_formula.values()):
+            if element not in {'hdi', 'H'}:
+                self.n_heavy_atoms += atoms
 
-  def reorder_molecular_formula(self):
-    self.str_molecular_formula = ''
-    for i_element in 'C','H','B','Br','Cl','F','I','N','O','P','S':
-      if self.molecular_formula[i_element] > 0:
-        if self.molecular_formula[i_element] == 1:
-          self.str_molecular_formula += f'{i_element}'
+    def ask_molecular_formula(self):
+        try:
+            self.str_molecular_formula = st.text_input("Enter the molecular formula (e.g. C6H6, C2H5NO2): ", value="C6H14", key="first")
+            self.opt_carbenes = st.checkbox("Search with carbenes")
+            self.get_molecular_formula()
+            self.is_valid_molecular_formula = True
+            self.count_atoms_from_molecular_formula(self.molecular_formula)
+        except Exception:
+            pass
+
+    def get_molecular_formula(self):
+        elements_y_atoms = re.findall(r'([A-Z][a-z]*)(\d*)', self.str_molecular_formula)
+        if elements_y_atoms != []:
+            self.molecular_formula = dict()
+            for i_tuple in elements_y_atoms:
+                if i_tuple[1] == '':
+                    self.molecular_formula[i_tuple[0]] = 1
+                else:
+                    self.molecular_formula[i_tuple[0]] = int(i_tuple[1])
+            for i_element in 'C', 'H', 'N', 'O', 'S', 'B', 'P', 'F', 'Cl', 'Br', 'I':
+                if i_element not in self.molecular_formula:
+                    self.molecular_formula[i_element] = 0
+            self.molecular_formula['hdi'] = get_hdi(self.molecular_formula)
         else:
-          self.str_molecular_formula += f'{i_element}{self.molecular_formula[i_element]}'
-#----------------------------------------------------------------------------------- Section 2
-  def write_name_output_file(self):
-    label_carbenes = ""
-    if self.opt_carbenes:
-      label_carbenes = "_with_carbenes"
+            self.molecular_formula = None
 
-    self.filename_output_pkl = self.str_molecular_formula + label_carbenes + '.pkl'
-    self.filename_output_smi = self.str_molecular_formula + label_carbenes + '.smi'
-    self.filename_output_xyz = self.str_molecular_formula + label_carbenes + '.xyz'
-#----------------------------------------------------------------------------------- Section 3
-  def get_syntax_rules(self):
-    if self.n_heavy_atoms in {1, 2, 3}:
-      self.syntax_rules = [[0] for i_atom in range(self.n_heavy_atoms)]
-    elif self.n_heavy_atoms in {4, 5, 6}:
-      self.syntax_rules = [[0], [0]] + [[0, 1] for i_atom in range(self.n_heavy_atoms - 3)] + [[0]]
-    elif self.n_heavy_atoms == 7:
-      self.syntax_rules = [[0], [0],[0, 1], [0, 1, 2], [0, 1, 3], [0, 1], [0]]
-    elif self.n_heavy_atoms == 8:
-      self.syntax_rules = [[0], [0],[0, 1], [0, 1, 2], [0, 1, 2, 3], [0, 1, 3], [0, 1], [0]]
-    elif self.n_heavy_atoms > 8:
-      self.syntax_rules = [[0], [0], [0, 1], [0, 1, 2], [0, 1, 2, 3]]
-      self.syntax_rules += [[0, 1, 2, 3] for i_atom in range(self.n_heavy_atoms - 8)]
-      self.syntax_rules += [[0, 1, 3], [0, 1], [0]]
-#----------------------------------------------------------------------------------- Section 3
-  def get_cycles_pi_systems(self):
-      self.cycles_pi_systems = get_unsaturations(self.molecular_formula, self.n_heavy_atoms)
+        # Get output vector elements
+        self.out_fm = []
+        for i_element in 'C', 'N', 'O', 'S', 'B', 'P', 'F', 'Cl', 'Br', 'I':
+            self.out_fm.append(self.molecular_formula[i_element])
+
+    def reorder_molecular_formula(self):
+        self.str_molecular_formula = ''
+        for i_element in 'C', 'H', 'B', 'Br', 'Cl', 'F', 'I', 'N', 'O', 'P', 'S':
+            if self.molecular_formula[i_element] > 0:
+                if self.molecular_formula[i_element] == 1:
+                    self.str_molecular_formula += f'{i_element}'
+                else:
+                    self.str_molecular_formula += f'{i_element}{self.molecular_formula[i_element]}'
+
+# ----------------------------------------------------------------------------------- Section 2
+    def write_name_output_file(self):
+        label_carbenes = ""
+        if self.opt_carbenes:
+            label_carbenes = "_with_carbenes"
+
+        self.filename_output_pkl = self.str_molecular_formula + label_carbenes + '.pkl'
+        self.filename_output_smi = self.str_molecular_formula + label_carbenes + '.smi'
+        self.filename_output_xyz = self.str_molecular_formula + label_carbenes + '.xyz'
+
+# ----------------------------------------------------------------------------------- Section 3
+    def get_syntax_rules(self):
+        if self.n_heavy_atoms in {1, 2, 3}:
+            self.syntax_rules = [[0] for i_atom in range(self.n_heavy_atoms)]
+        elif self.n_heavy_atoms in {4, 5, 6}:
+            self.syntax_rules = [[0], [0]] + [[0, 1] for i_atom in range(self.n_heavy_atoms - 3)] + [[0]]
+        elif self.n_heavy_atoms == 7:
+            self.syntax_rules = [[0], [0], [0, 1], [0, 1, 2], [0, 1, 3], [0, 1], [0]]
+        elif self.n_heavy_atoms == 8:
+            self.syntax_rules = [[0], [0], [0, 1], [0, 1, 2], [0, 1, 2, 3], [0, 1, 3], [0, 1], [0]]
+        elif self.n_heavy_atoms > 8:
+            self.syntax_rules = [[0], [0], [0, 1], [0, 1, 2], [0, 1, 2, 3]]
+            self.syntax_rules += [[0, 1, 2, 3] for i_atom in range(self.n_heavy_atoms - 8)]
+            self.syntax_rules += [[0, 1, 3], [0, 1], [0]]
+
+# ----------------------------------------------------------------------------------- Section 4
+    def get_cycles_pi_systems(self):
+        self.cycles_pi_systems = get_unsaturations(self.molecular_formula, self.n_heavy_atoms)
